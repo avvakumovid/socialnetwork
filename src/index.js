c75d4525c4fd968a5914d65dcc8b96cec438ff6f
avvakumovid <avvakumovid@gmail.com>
2021-09-16 12:29:57 +0300

    Добавление счетчика лайков к Post.jsx

--- src/index.js
+++ src/index.js
@@ -12,27 +12,27 @@ let postData = [
         id: 1,
         src: 'https://image.flaticon.com/icons/png/512/2077/2077407.png',
         text: 'Lorem ipsum dolor.',
-        likeCount: 10
+        likeCount: 14
     },
     {
         id: 2,
         src: 'https://image.flaticon.com/icons/png/512/2077/2077402.png',
         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.\n' +
             '                  Laboriosam, libero.',
-        likeCount: 10
+        likeCount: 19
     },
     {
         id: 3,
         src: 'https://image.flaticon.com/icons/png/512/2077/2077407.png',
         text: 'Lorem, ipsum dolor sit amet consectetur adipisicing.',
-        likeCount: 10
+        likeCount: 8
     },
     {
         id: 4,
         src: 'https://image.flaticon.com/icons/png/512/2077/2077402.png',
         text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.\n' +
             '                  Laboriosam, libero.',
-        likeCount: 10
+        likeCount: 22
     },
 ]
 
