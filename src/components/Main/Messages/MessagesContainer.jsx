1d352084650f40303b590e377683aca19f33b5b0
avvakumovid <avvakumovid@gmail.com>
2021-10-01 12:32:27 +0300

    Добавление контекста и рефакторинг кода

--- src/components/Main/Messages/MessagesContainer.jsx
+++ src/components/Main/Messages/MessagesContainer.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import {sendMessageActionCreator, updateNewMessageTextActionCreator} from "../../../redux/actionCreators";
 import Messages from "./Messages";
+import StoreContext from "../../../StoreContext";
 
 
 const MessagesContainer = (props) => {
 
-    let state = props.store.getState();
-    let messages = state.messagesPage.messages;
-    let dialogs = state.messagesPage.dialogs;
-    let newMessageText = state.messagesPage.newMessageText;
-    let sendMessage = () => {
-        props.store.dispatch(sendMessageActionCreator());
-    }
 
-    let updateNewMessageText = (textMessage) => {
-        props.store.dispatch(updateNewMessageTextActionCreator(textMessage));
-    }
+    return (
+        <StoreContext.Consumer>
+            { (store) =>
+            {
 
-    return ( <Messages sendMessage={sendMessage}  updateNewMessageText={updateNewMessageText} messages={messages} dialogs={dialogs} newMessageText={newMessageText} />);
+                let state = store.getState();
+                let messages = state.messagesPage.messages;
+                let dialogs = state.messagesPage.dialogs;
+                let newMessageText = state.messagesPage.newMessageText;
+                let sendMessage = () => {
+                    store.dispatch(sendMessageActionCreator());
+                }
+
+                let updateNewMessageText = (textMessage) => {
+                    store.dispatch(updateNewMessageTextActionCreator(textMessage));
+                }
+
+               return <Messages sendMessage={sendMessage} updateNewMessageText={updateNewMessageText} messages={messages}
+                          dialogs={dialogs} newMessageText={newMessageText}/>
+            }
+
+            }
+        </StoreContext.Consumer>
+    );
 }
 export default MessagesContainer;
