c8fce999b5082ad3faec2343a2591f9252f0d634
avvakumovid <avvakumovid@gmail.com>
2021-10-04 16:46:32 +0300

    Добавление страницы Users.jsx

--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -5,6 +5,7 @@ const Users = (props) => {
         props.setUser([{id: 1, profileIamge: 'https://cdn-icons-png.flaticon.com/512/5275/5275147.png', followed: true, firstName: 'Ivan', lastName: 'Avvakumov', status: 'The Best!!!', loaction: {city: "Zvenigorod", country: "Russia"}},
             {id: 2, profileIamge: 'https://cdn-icons-png.flaticon.com/512/5275/5275305.png', followed: false, firstName: 'Anton', lastName: 'Ivanov', status: 'The Beast!!!', loaction: {city: "Moscow", country: "Russia"}},
             {id: 3, profileIamge: 'https://cdn-icons-png.flaticon.com/512/5275/5275157.png', followed: true, firstName: 'Sam', lastName: 'Fisher', status: 'SAD', loaction: {city: "London", country: "England"}},
+
         ])
     }
     return (<div>
