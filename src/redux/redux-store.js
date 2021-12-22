fd5f4e4ff8f098e3a6c77b354f548c37a6ad60ab
Avvakumov Ivan <avvakumovid@gmail.com>
2021-12-22 20:07:32 +0300

    Настройка redux-store.js для расшиерния в браузере

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,10 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import profileReducer from "./profileReducer";
 import messagesReducer from "./messagesReducer";
 import sidebarReducer from "./sidebarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
-import thunkMiddlewaer from "redux-thunk"
+import thunkMiddleware from "redux-thunk"
 import {reducer as formReducer} from 'redux-form'
 import appReducer from "./appReducer";
 
@@ -19,7 +19,11 @@ let reducers = combineReducers({
     app: appReducer
 })
 
-let store = createStore(reducers, applyMiddleware(thunkMiddlewaer));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
+// let store = createStore(reducers, applyMiddleware(thunkMiddlewaer));
 
 window.store = store;
 
